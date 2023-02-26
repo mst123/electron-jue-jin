2e51dfb0cdf30270a2a3be7ae47857e0f8d5fe09
mashitu <624952546@qq.com>
2023-02-26 14:41:08 +0800

    feature(all): 添加路由

--- src/main/mainEntry.ts
+++ src/main/mainEntry.ts
@@ -1,30 +1,38 @@
-import { app, BrowserWindow } from "electron";
-import { CustomScheme } from './CustomScheme';
+import { app, autoUpdater, BrowserWindow } from "electron";
+import { CustomScheme } from "./CustomScheme";
+import { Updater } from "./Updater";
+import { CommonWindowEvent } from "./CommonWindowEvent";
 // 用于设置渲染进程开发者调试工具的警告，这里设置为 true 就不会再显示任何警告了
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = "true";
+app.on("browser-window-created", (e, win) => {
+  CommonWindowEvent.regWinEvent(win);
+});
 let mainWindow: BrowserWindow;
-
 app.whenReady().then(() => {
   let config = {
+    frame: false,
     webPreferences: {
-      nodeIntegration: true, //把 Node.js 环境集成到渲染进程中
+      nodeIntegration: true,
       webSecurity: false,
       allowRunningInsecureContent: true,
-      contextIsolation: false, // 在同一个 JavaScript 上下文中使用 Electron API
+      contextIsolation: false,
       webviewTag: true,
       spellcheck: false,
       disableHtmlFullscreenWindowResize: true,
     },
   };
+
   mainWindow = new BrowserWindow(config);
   // 打开开发者调试工具
   mainWindow.webContents.openDevTools({ mode: "undocked" });
-  
   if (process.argv[2]) {
     // 这里就是打开vue前端页面啦
     mainWindow.loadURL(process.argv[2]);
   } else {
     CustomScheme.registerScheme();
     mainWindow.loadURL(`app://index.html`);
+    // Updater.check();
   }
+  CommonWindowEvent.listen();
+  CommonWindowEvent.regWinEvent(mainWindow);
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { devPlugin, getReplacer } from "./plugins/devPlugin";
-import { buildPlugin } from "./plugins/buildPlugin";
 import optimizer from "vite-plugin-optimizer";
 
 export default defineConfig({
   plugins: [optimizer(getReplacer()), devPlugin(), vue()],
-  build: {
-    rollupOptions: {
-      plugins: [buildPlugin()],
-    },
-  },
-
 });
